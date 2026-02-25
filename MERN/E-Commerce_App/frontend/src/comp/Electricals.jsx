ee049dceda62b0167e0751bd7378e90e6b17d300
psiva-4321 <psivadurgaprsadpabbu@gmail.com>
2026-02-25 11:54:11 +0530

    Removed E-Commerce_App project

--- MERN/E-Commerce_App/frontend/src/comp/Electricals.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Electricals = () => {
-  return <div>Electricals</div>;
-};
-
-export default Electricals;
--- MERN/E-Commerce_App/frontend/src/comp/Mobiles.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Mobiles = () => {
-  return <div>Mobiles</div>;
-};
-
-export default Mobiles;
